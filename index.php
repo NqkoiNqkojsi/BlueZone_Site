d9fc84129979582efe57abdb010b9a789a3f7e4d
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2021-03-27 05:29:10 +0200

    aaaa

--- index.php
+++ index.php
@@ -108,16 +108,16 @@ body,
 <!--JS scrpts -->
 <script>
 console.log(data2)
-console.log(data2.streets[i].bound.lenght);
+console.log(data2.streets[0].bound.lenght);
 //Load the markers
 var br_mrk = 0;
 var mrk = new Array();
 var i = 0, j = 0, k = 0;
-br_mrk = data2.streets[i].bound.lenght;
 console.log(br_mrk);
 for (i = 0; i < data2.streets.lenght; i++) {
     for (j = 0; j < data2.streets[i].bound.lenght; j++) {
         mrk[j] = { lat: data2.streets[i].bound.marker[0], lng: data2.streets[i].bound.marker[1] };
+        br_mrk=br_mrk+1;
     }    
 }
 
